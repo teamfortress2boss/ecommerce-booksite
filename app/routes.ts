7057ed605b5d337954f491e2434b0d6a677829ec
Gemermemer <Arielbjorneberg13@gmail.com>
2025-11-25 12:40:12 -0600

    Add new routes for cart and checkout pages

--- app/routes.ts
+++ app/routes.ts
@@ -9,4 +9,8 @@ export default [index("routes/home.tsx"),
     route("price-match", "routes/price-match.tsx"),
     route("shop", "routes/shop.tsx"),
     route("signup", "routes/signup.tsx")
+    route("cart", "routes/Cart.tsx"),
+    route("checkout", "routes/Checkout.tsx"),
+    route("orders", "routes/OrderHistory.tsx"),
+    route("order-confirmation", "routes/OrderConfirmation.tsx"),
 ] satisfies RouteConfig;
